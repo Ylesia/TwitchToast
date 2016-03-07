1d2ea72c450c6141ff429e1d3ac0497ae78d7986
Ylesia <jeff.quaderi@gmail.com>
2016-03-07 17:04:29 +0100

    testMongo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+//MongoDB
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert')
 var url = 'mongodb://localhost:27017/test';
@@ -10,14 +11,15 @@ MongoClient.connect(url, function(err, db) {
   console.log("Connected correctly to server.");
   db.close();
 });
-
+//FINMONGODB
 app.use(express.static('./public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
  .use(function(req, res){ // Répond enfin
      res.send('Hello');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('newTitle', { text: 'Title ready !', timeout: 5 });
+  // Test pour TitleReady
+  //socket.emit('newTitle', { text: 'Title ready !', timeout: 5 });
   socket.on('setNewTitle', function (data) {
     console.log(data);
     io.sockets.emit('newTitle', data);
