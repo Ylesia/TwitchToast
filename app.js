2e54ae2e2f7de1be5f823038ecf0902d570ffe60
Ylesia <jeff.quaderi@gmail.com>
2016-03-17 16:51:05 +0100

    Bits

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert')
 var url = 'mongodb://localhost:27017/test';
 //var url = 'mongodb://192.168.0.29:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
 //FINMONGODB
 app.use(express.static('./public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
  .use(function(req, res){ // Répond enfin
