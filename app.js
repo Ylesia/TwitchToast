4074766fa5aa2aa0013bc38a7bfe98cfd7e677d8
Ylesia <jeff.quaderi@gmail.com>
2016-03-25 18:29:45 +0100

    test data 3

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 //MongoDB
-// var MongoClient = require('mongodb').MongoClient;
-// var assert = require('assert')
-// var url = 'mongodb://localhost:27017/test';
-////var url = 'mongodb://192.168.0.29:27017/test';
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server.");
-//   db.close();
-// });
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert')
+var url = 'mongodb://localhost:27017/test';
+//var url = 'mongodb://192.168.0.29:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
 //FINMONGODB
 
 // TEST IMPORTS
