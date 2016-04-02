f30c4f727dfda64c2d46f3508138f8a63d399071
Ylesia <jeff.quaderi@gmail.com>
2016-04-02 17:37:28 +0200

    v0.2

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 //MongoDB
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert')
-var url = 'mongodb://localhost:27017/test';
-//var url = 'mongodb://192.168.0.29:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
-//FINMONGODB
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert')
+// var url = 'mongodb://localhost:27017/test';
+// //var url = 'mongodb://192.168.0.29:27017/test';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
+// //FINMONGODB
 
-// TEST IMPORTS
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
+// // TEST IMPORTS
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
+// var ObjectId = require('mongodb').ObjectID;
+// var url = 'mongodb://localhost:27017/test';
 //FIN TESTS IMPORTS
 
 app.use(express.static('./public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
