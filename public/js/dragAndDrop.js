e210a910c4d3732cf8ea5c0df3e2654c10b51920
Kevin Le Perf <codlabtech@gmail.com>
2016-07-04 23:57:43 +0200

    switch adminBeta to material design and add support numeric in duration

--- public/js/dragAndDrop.js
+++ public/js/dragAndDrop.js
@@ -1,27 +1,47 @@
+/**
+ *
+ * revision 2 : change to manage ul/li behaviour by default
+ */
 var source;
 
+function getFirstParentMatching(node, typeString){
+  console.log(node.nodeName+" "+typeString);
+  var parent = node;
+  if (node && typeString) {
+    if (parent && parent.nodeName.toLowerCase() == typeString)
+      return parent;
+
+    do{
+      parent = parent.parentNode;
+    } while (parent && parent.nodeName.toLowerCase() != typeString);
+  }
+  return parent;
+}
+
 function isbefore(a, b) {
-    if (a.parentNode == b.parentNode) {
+  if (a.parentNode == b.parentNode) {
     for (var cur = a; cur; cur = cur.previousSibling) {
-        if (cur === b) { 
+      if (cur === b) {
         return true;
-        }
+      }
     }
-    }
-    return false;
-} 
+  }
+  return false;
+}
 
 function dragenter(e) {
-    if (isbefore(source, e.target)) {
-    e.target.parentNode.insertBefore(source, e.target);
-    }
-    else {
-    e.target.parentNode.insertBefore(source, e.target.nextSibling);
-    }
+  var parent = getFirstParentMatching(e.target, "ul");
+  if (!parent) parent = e.target.parentNode;
+
+  var li = getFirstParentMatching(e.target, "li");
+  if (isbefore(source, li)) {
+    parent.insertBefore(source, li);
+  }else{
+    parent.insertBefore(source, li.nextSibling);
+  }
 }
 
 function dragstart(e) {
-    source = e.target;
-    e.dataTransfer.effectAllowed = 'move';
+  source = getFirstParentMatching(e.target, "li");
+  e.dataTransfer.effectAllowed = 'move';
 }
-
